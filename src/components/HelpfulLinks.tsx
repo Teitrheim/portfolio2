8488610c4c04954d61ba71638554294f5b03e222
Thomas Eitrheim <teitrheim@gmail.com>
2024-10-07 15:33:05 +0200

    Refactor HelpfulLinks component and add title attributes to social media links

--- src/components/HelpfulLinks.tsx
+++ src/components/HelpfulLinks.tsx
@@ -8,6 +8,7 @@ const HelpfulLinks: React.FC = () => {
         href="https://github.com/Teitrheim"
         target="_blank"
         rel="noopener noreferrer"
+        title="GitHub Profile"
       >
         <i className="fab fa-github helpful-icon"></i>
       </a>
@@ -15,10 +16,11 @@ const HelpfulLinks: React.FC = () => {
         href="https://www.linkedin.com/"
         target="_blank"
         rel="noopener noreferrer"
+        title="LinkedIn Profile"
       >
         <i className="fab fa-linkedin helpful-icon"></i>
       </a>
-      <a href="mailto:teitrheim@gmail.com">
+      <a href="mailto:teitrheim@gmail.com" title="Send Email">
         <i className="fas fa-envelope helpful-icon"></i>
       </a>
     </div>
